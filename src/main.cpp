0879f93cc5e0a263027ad9bfbb5883ec08c3db32
Uday Pratap <udaypratap@Udays-MacBook-Air.local>
2026-05-27 19:52:15 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,10 @@ int main() {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
+    if(input == "exit") {
+      return 0;
+    }
     std::cout << input << ": command not found" << std::endl;
   }
-  return 0;
+  return 1;
 }
