fa21331c3a9a049dc75e2e9d02c5a8251ab0f133
Uday Pratap <udaypratap@Udays-MacBook-Air.local>
2026-05-27 23:48:49 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,19 @@
 #include <string>
 #include <sstream>
 #include <unordered_set>
+#include <unistd.h>
 
+#ifdef _WIN32
+constexpr char PATH_LIST_SEPARATOR = ';';
+#else
+constexpr char PATH_LIST_SEPARATOR = ':';
+#endif
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
   while(true) {
     std::cout << "$ ";
     std::string user_input;
@@ -30,9 +35,25 @@ int main() {
       std::stringstream ss(args);
       std::string arg;
       while(ss >> arg) {
+        //built in command
         if(built_in_commands.find(arg) != built_in_commands.end()) {
           std::cout << arg << " is a shell builtin" << std::endl;
         }else {
+          //not a built in command, search for binary in PATH
+          char* path_env = std::getenv("PATH");
+          if(path_env != nullptr) { 
+            std::stringstream path_ss(path_env);
+            std::string path;
+            while(std::getline(path_ss, path, PATH_LIST_SEPARATOR)) {
+              std::string exec_path = path + "/" + arg;
+              // 0 means file exists and has exec permission
+              // -1 means no file/no exec permission
+              if(access(exec_path.c_str(), X_OK) == 0) {
+                std::cout << arg << " is " << exec_path << std::endl;
+              }
+            }
+          }
+          //neither a built in command nor a executable in PATH. Print not found.
           std::cout << arg << ": not found" << std::endl;
         }
       }
