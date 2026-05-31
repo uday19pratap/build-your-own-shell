5f2fd801c7582ebf213217ad8e56901b0114fb2a
Uday Pratap <udaypratap@Udays-MacBook-Air.local>
2026-06-01 03:35:55 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ constexpr char PATH_LIST_SEPARATOR = ';';
 #else
 constexpr char PATH_LIST_SEPARATOR = ':';
 #endif
-const std::unordered_set<std::string> built_in_commands = {"echo", "type", "exit"};
+const std::unordered_set<std::string> built_in_commands = {"echo", "type", "exit", "complete"};
 
 struct ParsedCommand {
   std::string command;
