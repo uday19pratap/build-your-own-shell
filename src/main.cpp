907a33d4b9d2240444b07f2f5d9365948455d0ff
Uday Pratap <udaypratap@Udays-MacBook-Air.local>
2026-06-02 21:44:17 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -385,12 +385,11 @@ std::set<std::string> run_completer_script(std::string user_input) {
   std::string exec = (it->second) + " " + parsed_command.command;
   const std::vector<std::string>& args = parsed_command.args;
   if(args.size() > 0) {
-    exec += args.back();
+    exec += " " + args.back() + " ";
   }
   if(args.size() > 1) {
     exec += args[args.size() - 2];
   }
-  std::cout << std::endl << "EXEC:  " << exec.c_str() << std::endl;
   // "r" mode makes sure you can only read from the pipe
   // the child process triggered with completer_script, whatever it writes onto its std out stream
   // we can read from it only.. with "w" we could write to child's input stream and drive it through
